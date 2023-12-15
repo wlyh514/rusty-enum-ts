6c95ebf6a6b644cf03360af5056d08a39af00035
York <57304851+wlyh514@users.noreply.github.com>
2023-12-14 23:34:55 -0500

    fix: export Result and Option from index.ts

--- src/index.ts
+++ src/index.ts
@@ -6,6 +6,7 @@ import type { EnumType } from "./core";
 import { match, ifLet } from "./utils";
 import { asyncMatch, intoOptionPromise, intoResultPromise } from "./async";
 import type { EnumPromise } from "./async";
+import type { Result, Option } from "./enums";
 
 export { enumFactory as Enum, match, ifLet, asyncMatch, intoOptionPromise, intoResultPromise };
-export type { EnumType, EnumPromise };
+export type { EnumType, EnumPromise, Result, Option };
